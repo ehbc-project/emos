8c090653917bbd7c14bb397f4e20e1d776a38f5a
kms1212 <kms1212g@gmail.com>
2025-05-03 01:05:16 +0900

    added: GNU Unifont rendering

--- boot/arch/i686/pc/init.c
+++ boot/arch/i686/pc/init.c
@@ -17,6 +17,9 @@
 
 extern void main(void);
 
+extern int font_is_glyph_full_width(uint32_t codepoint);
+extern int font_get_glyph(uint32_t codepoint, uint8_t *buf, long size);
+
 struct pci_device pci_devices[32];
 
 static void print_hex_dword(uint32_t value)
@@ -24,6 +27,55 @@ static void print_hex_dword(uint32_t value)
     static const char hex_chars[] = "0123456789ABCDEF";
     for (int i = 0; i < 8; i++) {
         _pc_bios_tty_output(hex_chars[(value >> (28 - i * 4)) & 0xF]);
+    }
+}
+
+static void print_glyph(struct vbe_video_mode_info *vbe_mode_info, uint32_t codepoint, uint16_t x_offset, uint16_t y_offset)
+{
+    int is_full_width = font_is_glyph_full_width(codepoint);
+    uint8_t glyph[32];
+
+    if (font_get_glyph(codepoint, glyph, 32)) return;
+
+    uint8_t *framebuffer = (uint8_t*)(vbe_mode_info->framebuffer);
+
+    for (int y = y_offset; y < y_offset + 16; y++) {
+        for (int x = x_offset; x < x_offset + (is_full_width ? 16 : 8); x++) {
+            uint8_t current_byte = glyph[(y - y_offset) * (is_full_width ? 2 : 1) + (x - x_offset) / 8];
+            int on = current_byte & (0x80 >> ((x - x_offset) % 8));
+            framebuffer[y * vbe_mode_info->pitch + x * vbe_mode_info->bpp / 8 + 0] = on ?  0xFF : 0x00;
+            framebuffer[y * vbe_mode_info->pitch + x * vbe_mode_info->bpp / 8 + 1] = on ?  0xFF : 0x00;
+            framebuffer[y * vbe_mode_info->pitch + x * vbe_mode_info->bpp / 8 + 2] = on ?  0xFF : 0x00;
+        }
+    }
+}
+
+static void echo_char(struct vbe_video_mode_info *vbe_mode_info)
+{
+    int cur_x = 0, cur_y = 0;
+
+    for (;;) {
+        char ch;
+        _pc_bios_read_keyboard(NULL, &ch);
+        if (ch == '\r') {
+            _pc_bios_tty_output('\n');
+            cur_y++;
+            cur_x = 0;
+        } else {
+            int is_full_width = font_is_glyph_full_width(ch);
+            if (is_full_width && cur_x + 2 > 80) {
+                cur_y++;
+                cur_x = 0;
+            }
+
+            print_glyph(vbe_mode_info, ch, cur_x * 8, cur_y * 16);
+            cur_x += is_full_width ? 2 : 1;
+
+            if (cur_x >= 80) {
+                cur_y++;
+                cur_x = 0;
+            }
+        }
     }
 }
 
@@ -35,7 +87,7 @@ void _pc_init(void)
         _pc_bios_tty_output(str[i]);
     }
 
-    _pc_bios_read_drive_chs(_pc_boot_drive, (struct chs){ 0, 0, 1 }, 18, (void*)0x10000);
+    _pc_bios_read_drive_chs(_pc_boot_drive, (struct chs){ 0, 1, 1 }, 9, (void*)0xA000);
 
     _bus_pci_host_scan(pci_devices, 32);
 
@@ -81,23 +133,7 @@ void _pc_init(void)
 
     _pc_bios_set_vbe_video_mode(mode);
 
-    uint8_t *framebuffer = (uint8_t*)(vbe_mode_info.framebuffer);
-    for (int y = 0; y < vbe_mode_info.height; y++) {
-        for (int x = 0; x < vbe_mode_info.width; x++) {
-            framebuffer[y * vbe_mode_info.pitch + x * vbe_mode_info.bpp / 8 + 0] = 0xFF;
-            framebuffer[y * vbe_mode_info.pitch + x * vbe_mode_info.bpp / 8 + 1] = 0xFF;
-            framebuffer[y * vbe_mode_info.pitch + x * vbe_mode_info.bpp / 8 + 2] = 0x00;
-        }
-    }
-
-    for (;;) {
-        char ch;
-        _pc_bios_read_keyboard(NULL, &ch);
-        if (ch == '\r') {
-            _pc_bios_tty_output('\n');
-        }
-        _pc_bios_tty_output(ch);
-    }
+    echo_char(&vbe_mode_info);
 
     main();
 
